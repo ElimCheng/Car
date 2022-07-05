3acf22fb4ebfc1a041e9e4125eacd9157c2cab42
Elim <elimbook@gmail.com>
2022-07-05 21:12:15 +0800

    car & road

---
+++ car.js
@@ -0,0 +1,97 @@
+class Car{
+    constructor(x,y,width,height){
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+
+    this.speed=0;
+    this.acceleration=0.2;
+    this.maxSpeed=3;
+    this.friction=0.05;
+    this.angle=0;
+
+    this.controls=new Controls();
+    }
+
+    update(){
+        this.#move();
+    }
+
+    #move(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
+        }
+
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
+    }
+
+    draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
+        ctx.beginPath();
+        ctx.fillStyle = "#004B97";
+        ctx.rect(
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.fillStyle = "#FFF0AC";
+        ctx.rect(
+            -this.width/2,
+            -this.height/2,
+            this.width/3,
+            this.height/6
+            );
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.fillStyle = "#96FED1";
+        ctx.rect(
+            +this.width/6,
+            -this.height/2,
+            this.width/3,
+            this.height/6
+            );
+        ctx.fill();
+        
+        ctx.restore();
+    }
+}
